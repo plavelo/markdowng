5ebe375d14ea465ae293bfeda4fd6a278b4cd5b7
plavelo <plastic.velocity@gmail.com>
2013-06-02 22:41:53 +0900

    add convertHeaders tests

--- src/test/groovy/MarkdownSpec.groovy
+++ src/test/groovy/MarkdownSpec.groovy
@@ -1,9 +1,8 @@
 package test.groovy
 
-import org.plavelo.markdown.Markdown
-import spock.lang.*
+import org.plavelo.markdown.*
 
-class MarkdownSpec extends spock.lang.Specification {
+public class MarkdownSpec extends spock.lang.Specification {
 	
 	def setup() {
 	}
@@ -28,4 +27,38 @@ class MarkdownSpec extends spock.lang.Specification {
 		"\t\tsome\n\ttext\t" | "        some\n    text    "
 		"some text"          | "some text"
 	}
+
+    def "Convert headers"() {
+        expect:
+        new Markdown().convertHeaders(before) == converted
+
+        where:
+        before                     | converted
+        "setext style\n====="      | "<h1>setext style</h1>\n"
+        "setext style\n-------"    | "<h2>setext style</h2>\n"
+        "# atx style"              | "<h1>atx style</h1>\n"
+        "## atx style"             | "<h2>atx style</h2>\n"
+        "### atx style"            | "<h3>atx style</h3>\n"
+        "#### atx style"           | "<h4>atx style</h4>\n"
+        "##### atx style"          | "<h5>atx style</h5>\n"
+        "###### atx style"         | "<h6>atx style</h6>\n"
+        "####### atx style"        | "<h6># atx style</h6>\n"
+        "# atx style2 #"           | "<h1>atx style2</h1>\n"
+        "## atx style2 ##"         | "<h2>atx style2</h2>\n"
+        "### atx style2 ####"      | "<h3>atx style2</h3>\n"
+        "#### atx style2 ##"       | "<h4>atx style2</h4>\n"
+        "##### atx style2 #"       | "<h5>atx style2</h5>\n"
+        "###### atx style2 #####"  | "<h6>atx style2</h6>\n"
+        "####### atx style2 #####" | "<h6># atx style2</h6>\n"
+    }
+
+
+    def "Convert block quotes"() {
+        expect:
+        new Markdown().convertBlockQuotes(before) == converted
+
+        where:
+        before             | converted
+        "> some text"      | "<blockquote>some text</blockquote>\n"
+    }
 }
